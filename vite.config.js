b11c262296c4175f6f49aa10728100977a977009
Alexandre Lacour <alexandre.lacour-ext@ima.eu>
2025-03-11 10:25:41 +0100

    Update deployment configuration for GitHub Pages
    Updated the `homepage` URL and `deploy` script in `package.json` to align with changes in the project setup. Added a `base` configuration in `vite.config.js` to support the proper deployment path.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Important pour des chemins relatifs
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
-  base: '/MyPortfolio/', // Le nom de ton dépôt GitHub
 });
