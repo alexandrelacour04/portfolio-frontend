3c0e699cd889141c4f07653a084eea43b3c4e03c
Alexandre Lacour <alexandre.lacour-ext@ima.eu>
2025-03-12 16:38:52 +0100

    Refactor main app structure and simplify routing.
    Replaced complex route mapping with a functional component-based structure for improved readability and maintainability. Removed unused imports and legacy code, and added a new "/a-propos" route. Simplified navigation fallback for unknown routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,31 @@
 import React, {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+
 import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom'
 import './index.css'
 import Timeline from "./pages/timeline.jsx";
-import DefaultLayout from "./common/DefaultLayout.jsx";
+
 import HomePage from "./pages/home.jsx";
 import AdminDashBoard from "./admin/pages/AdminDashBoard.jsx";
-import {UserProvider} from "./utils/UserContext.jsx";
+
 import Projets from "./pages/projets.jsx";
-import PageTitle from "./common/pageTitle.jsx";
 
-const routes = [
-    {path: "/", element: <HomePage/>, title: "Accueil"},
-    {path: "/timeline", element: <Timeline/>, title: "Timeline"},
-    {path: "/admin", element: <AdminDashBoard/>, title: "Admin Dashboard"},
-    {path: "/projets", element: <Projets/>, title: "Projets" },
-];
 
-createRoot(document.getElementById('root')).render(
-    <UserProvider>
-        <StrictMode>
-            <Router>
-                <Routes>
-                    {routes.map((route, index) => (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={
-                                <DefaultLayout>
-                                    <PageTitle routes={routes}/>
-                                    {route.element}
-                                </DefaultLayout>
-                            }
-                        />
-                    ))}
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                </Routes>
-            </Router>
-        </StrictMode>
-    </UserProvider>
-)
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/timeline" element={<Timeline />} />
+                <Route path="/admin" element={<AdminDashBoard />} />
+                <Route path="/projets" element={<Projets />} />
+                <Route path="/a-propos" element={<Apropos />} />
+                {/* Redirige toutes les routes inconnues vers HomePage */}
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
